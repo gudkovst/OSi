f85d98909ad2b91396154e3f56b54ea224d478f9
gudkovst <69453922+gudkovst@users.noreply.github.com>
2022-09-06 22:48:37 +0800

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void* childFunc(void* args){
+	for (char c = 0; c < 10; c++)
+		printf("child string %d\n", c);
+	return NULL;
+}
+
+int main(){
+	pthread_t thr;
+	int st;
+	if (st = pthread_create(&thr, NULL, childFunc, NULL)){
+		printf("Not create thread: %d\n", st);
+		return st;
+	}
+	if (st = pthread_join(thr, NULL)){
+		printf("Not join thread: %d\n", st);
+		return st;
+	}
+	for (char c = 0; c < 10; c++)
+		printf("parent string %d\n", c);
+	return 0;
+}
